c9a589b8197372756007302d0621bc5ca5e0067c
Mohamed Romdhani <mromdhani7@gmail.com>
2020-05-19 08:57:18 +0200

    updated for 2nd session

--- Docker Training - SourceCode/DockerTraining-SourceCode/lab-03-step-2-simple-java-app/src/main/java/be/businesstraining/App.java
+++ Docker Training - SourceCode/DockerTraining-SourceCode/lab-03-step-2-simple-java-app/src/main/java/be/businesstraining/App.java
@@ -2,6 +2,6 @@ package be.businesstraining;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello, I am dockerized ...");
+		System.out.println("Hello, I am the Java dockerized App ...");
 	}
 }
--- Docker Training - SourceCode/DockerTraining-SourceCode/lab-05-step-3-compose-angular-springboot-mysql/backend-springboot-mysql/src/main/java/be/businesstraining/repsoitory/IProductsRepository.java
+++
@@ -1,9 +0,0 @@
-package be.businesstraining.repsoitory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import be.businesstraining.domain.Product;
-
-public interface IProductsRepository extends JpaRepository<Product, String> {
-
-}
